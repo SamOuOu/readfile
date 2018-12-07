0cd702c49586ba4f2fdb63334aee5193c2d13014
Sam Ou <lll810502@gmail.com>
2018-12-07 15:19:18 +0800

    fliter

--- readfile.py
+++ readfile.py
@@ -6,7 +6,15 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0:
             print(len(data))
-print(len(data))
+print('檔案讀取完畢，共有', len(data), '筆資料')
 
-print(data[0])
+sum_len = 0
+for length in data:
+    sum_len = (sum_len + len(length))
+print(sum_len / len(data))
 
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print(len(new))            
